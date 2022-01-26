dddc263e73cdab1d0111af505f9be134ad5f0422
Riswan Agam <riswanagam@gmail.com>
2022-01-26 18:54:28 +0700

    update
    - Performance app using Canary
    - Security
    - Obfuscation

--- core/src/main/java/com/example/core/di/CoreModule.kt
+++ core/src/main/java/com/example/core/di/CoreModule.kt
@@ -10,17 +10,29 @@ import com.example.core.data.source.remote.RemoteDataSourceImpl
 import com.example.core.data.source.remote.network.ApiService
 import com.example.core.domain.repository.CatalogueRepositoryImpl
 import com.example.core.utils.BASE_URL
+import com.example.core.utils.SSLCertificateConfigurator
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.X509TrustManager
 
 var networkModule = module{
     single {
+        val trustManagerFactory = SSLCertificateConfigurator.getTrustManager(context = androidContext())
+        val trustManagers = trustManagerFactory.trustManagers
+        if(trustManagers.size != 1 || trustManagers[0] !is X509TrustManager) {
+            throw IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers))
+        }
+        val trustManager = trustManagers[0] as X509TrustManager
         OkHttpClient.Builder()
+            .sslSocketFactory(SSLCertificateConfigurator.getSSLConfiguration(androidContext()).socketFactory, trustManager)
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
@@ -39,10 +51,14 @@ var networkModule = module{
 var databaseModule = module {
     factory { get<CatalogueDatabase>().catalogueDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("moviecatalogue".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             CatalogueDatabase::class.java, "MovieCatalogue.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
